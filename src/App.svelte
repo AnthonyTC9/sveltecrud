351a0b60c2c53e1e38d0a9db0213810ae37f4367
Intern 1 <intern1@iMac-de-Eden.local>
2022-12-21 14:37:08 -0700

    todo

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { todos } from './stores'
+
+  import AddTodo from "./components/AddTodo.svelte"
+  import Todo from "./components/Todo.svelte"
+    import Toast from "./components/Toast.svelte"
+  //let todos = [];
+
+  if (localStorage.getItem("todos")) {
+    todos.local(JSON.parse(localStorage.getItem("todos")))
+  }
+
+  $: localStorage.setItem("todos", JSON.stringify($todos));
+  
+</script>
+
+<div class="container">
+  <h1 class="display-5 my-3">CRUD</h1>
+  <AddTodo />
+  {#each $todos as item}
+    <Todo {item} />
+  {/each}
+  <Toast />
+
+</div>
